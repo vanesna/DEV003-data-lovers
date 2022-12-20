6a25418754dda7abe22684c552fb2bc144f13d57
vanessa <vanne.sna@gmail.com>
2022-12-20 12:52:35 -0600

    Funcionamiento boton personajes

--- src/main.js
+++ src/main.js
@@ -4,3 +4,36 @@ import data from './data/harrypotter/data.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
 console.log(example, data);
+
+
+let names = [];
+
+let nameList="";
+
+document.getElementById("characters").addEventListener("click",showCharacters);
+
+function mapData(){
+  
+  const dataCharacters = data.characters;
+    
+  names = dataCharacters.map(({name}) => ({name}));
+
+  console.log(names);
+
+  
+}
+
+
+function showCharacters(){
+  nameList=document.getElementById("main");
+
+  names.forEach(function(data){
+    const linew= document.createElement("p");  
+    const contenido = document.createTextNode(data.name);
+    nameList.appendChild(linew);
+    nameList.appendChild(contenido);
+  })
+
+}
+
+mapData();
