67fe8001913693ba5430daf0bb067038133295ed
Patricia <patricia.ct29@gmail.com>
2023-01-11 23:22:58 -0500

    card in books

--- src/data.js
+++ src/data.js
@@ -2,16 +2,16 @@
 
 //Filtro por género
 export const filterGender = (dataCharacters,filterSelected) => {
-  const nuevoArray = dataCharacters.filter((charac) => charac.gender === filterSelected);
-  //console.log(nuevoArray);
+  const nuevoArray = dataCharacters.filter((charac) => charac.gender === filterSelected); 
+  console.log(nuevoArray);
   return nuevoArray;
-
 }
 
 //Filtro por casa de estudio
 export const filterHouse = (dataCharacters,houseSelected) => {
-  const nuevoArray = dataCharacters.filter((charac) => charac.house === houseSelected);
+  const nuevoArray = dataCharacters.filter((charac) => charac.house === houseSelected); 
   //console.log(nuevoArray);
   return nuevoArray;
-
+  
 }
+
